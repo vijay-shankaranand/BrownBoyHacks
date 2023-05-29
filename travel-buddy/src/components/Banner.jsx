61577c74f8b8337410aa88d2aa0e61a695b4ef44
Mohammed Faiz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-05-29 16:19:42 +0700

    formatting

--- travel-buddy/src/components/Banner.jsx
+++ travel-buddy/src/components/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
         <div className='banner'>
             <img src={familyImage}/>
             <div>
-                <h2>This could be your family</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque ipsa itaque, ipsam, quibusdam dolor tenetur molestiae asperiores nam rem, libero eveniet. Illum sint porro repudiandae impedit officiis. Saepe, a maxime.</p>
+                <h2 style={{ textAlign: 'center', width: '300px' }}>This could be your family!</h2>
+                <p style={{ textAlign: 'center', width: '300px' }}>Imagine lounging in a sun-drenched vineyard in Tuscany, glass of Chianti in hand, or savoring the exhilarating views of the pristine waters from a cliffside café on the Amalfi Coast. Dreaming of an Italian vacation? The flavors, the culture, the views – it's all within reach with TravelBuddy. It takes the 'planning' out of your vacation planning. With TravelBuddy, the endless possibilities of your next Italian escapade are just a few taps away. Andiamo!</p>
             </div>
         </div>
     );
